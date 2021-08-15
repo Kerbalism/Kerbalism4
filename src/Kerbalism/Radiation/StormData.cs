1898cd2866bc0b7541aacc95ed5b8ee09a349790
DRVeyl <DRVeyl@gmail.com>
2021-08-15 19:40:55 -0400

    Create StormState enum
    Don't rely on knowing magic numbers
    Remove deprecated code to translate storm time/generation values

--- src/Kerbalism/Radiation/StormData.cs
+++ src/Kerbalism/Radiation/StormData.cs
@@ -1,19 +1,13 @@
-using System;
-using System.Collections.Generic;
-using UnityEngine;
-
-
 namespace KERBALISM
 {
-
 	public class StormData
 	{
 		public void Reset()
 		{
 			storm_time = 0.0;
 			storm_duration = 0.0;
-			storm_state = 0;
-			msg_storm = 0;
+			storm_state = StormState.none;
+			msg_storm = StormState.none;
 			displayed_duration = 0;
 			display_warning = true;
 		}
@@ -30,19 +24,10 @@ namespace KERBALISM
 			storm_time = Lib.ConfigValue(node, "storm_time", 0.0);
 			storm_duration = Lib.ConfigValue(node, "storm_duration", 0.0);
 			storm_generation = Lib.ConfigValue(node, "storm_generation", 0.0);
-			storm_state = Lib.ConfigValue(node, "storm_state", 0u);
-			msg_storm = Lib.ConfigValue(node, "msg_storm", 0u);
+			storm_state = Lib.ConfigValue(node, "storm_state", StormState.none);
+			msg_storm = Lib.ConfigValue(node, "msg_storm", StormState.none);
 			displayed_duration = Lib.ConfigValue(node, "displayed_duration", storm_duration);
 			display_warning = Lib.ConfigValue(node, "display_warning", true);
-
-			if(storm_time > 0 && storm_duration <= 0) // legacy save games did storms differently.
-			{
-				// storm_time used to be the point of time of the next storm, there was no storm_duration
-				// as storms all had a fixed duration depending on the distance to the sun.
-				// setting this to 0 will trigger a reroll of storm generation based on the new implementation.
-				storm_time = 0;
-				storm_generation = 0;
-			}
 		}
 
 		public void Save(ConfigNode node)
@@ -59,8 +44,9 @@ namespace KERBALISM
 		public double storm_time;        // time of next storm
 		public double storm_duration;    // duration of current/next storm
 		public double storm_generation;  // time of next storm generation roll
-		public uint storm_state;         // 0: none, 1: inbound, 2: inprogress
-		public uint msg_storm;           // message flag
+		public enum StormState : uint { none = 0, inbound = 1, inprogress = 2 };
+		public StormState storm_state;
+		public StormState msg_storm;           // message flag
 
 		public double displayed_duration;
 		public bool display_warning;
