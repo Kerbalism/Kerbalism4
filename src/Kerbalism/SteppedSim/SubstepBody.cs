5f386ff35d14ab6ac0a087c0a7facd281b4461a1
DRVeyl <DRVeyl@gmail.com>
2021-08-28 13:30:29 -0400

    Tweak body occlusion code

--- src/Kerbalism/SteppedSim/SubstepBody.cs
+++ src/Kerbalism/SteppedSim/SubstepBody.cs
@@ -19,22 +19,22 @@ namespace KERBALISM.SteppedSim
 			//  mag(bv) if bv dot ab > 0
 			//  mag(ab cross av) / mag(ab) otherwise
 			double3 ab = b - a;
-			if (Unity.Burst.CompilerServices.Hint.Unlikely(math.lengthsq(ab) < 1))
+			var abLen2 = math.lengthsq(ab);
+			if (Unity.Burst.CompilerServices.Hint.Unlikely(abLen2 < 1))
 				return false;
 			bool occluded = false;
 			for (int i=0; i<occluders.Length; i++)
 			{
 				double3 v = occluders[i].position;
+				var radiusSq = occluders[i].radius * occluders[i].radius;
 				double3 av = v - a;
 				double3 bv = v - b;
-				double dist;
 				if (math.dot(av, ab) < 0)
-					dist = math.length(av);
+					occluded |= math.lengthsq(av) <= radiusSq;
 				else if (math.dot(bv, ab) > 0)
-					dist = math.length(bv);
+					occluded |= math.lengthsq(bv) <= radiusSq;
 				else
-					dist = math.length(math.cross(ab, av)) / math.length(ab);
-				occluded |= dist <= occluders[i].radius;
+					occluded |= math.lengthsq(math.cross(ab, av)) <= radiusSq * abLen2;
 			}
 			return occluded;
 		}
